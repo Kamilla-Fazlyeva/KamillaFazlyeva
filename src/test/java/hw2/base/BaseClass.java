9e91a9492db928861922084702e9d1e3e8a2ae10
Kamilla-Fazlyeva <flora666@yandex.ru>
2020-03-28 13:20:10 +0300

    Base class is added

---
+++ src/test/java/hw2/base/BaseClass.java
@@ -0,0 +1,28 @@
+package hw2.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+    }
+
+    // 12. Close Browser
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
