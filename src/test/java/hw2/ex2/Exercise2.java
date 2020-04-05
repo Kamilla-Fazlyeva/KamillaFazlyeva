fe46b1b5990128c59d4c75e47f71bab62af18ac3
Kamilla-Fazlyeva <57841579+Kamilla-Fazlyeva@users.noreply.github.com>
2020-04-05 22:44:09 +0300

    Update Exercise2.java

--- src/test/java/hw2/ex2/Exercise2.java
+++ src/test/java/hw2/ex2/Exercise2.java
@@ -41,45 +41,45 @@ public class Exercise2 extends BaseClass {
 
         // 6. Select checkboxes
         WebElement waterCheckbox = driver.findElement(By
-                .xpath("//label[@class='label-checkbox' and contains(.,'Water')]"));
+                .xpath("//label[@class='label-checkbox' and contains(.,'Water')]//input"));
         if (!waterCheckbox.isSelected()) {
             waterCheckbox.click();
         }
         WebElement windCheckbox = driver.findElement(By
-                .xpath("//label[@class='label-checkbox' and contains(., 'Wind')]"));
+                .xpath("//label[@class='label-checkbox' and contains(., 'Wind')]//input"));
         if (!windCheckbox.isSelected()) {
             windCheckbox.click();
         }
 
         // 7. Select radio
         WebElement selenRadio = driver.findElement(By
-                .xpath("//label[@class='label-radio' and contains(., 'Selen')]"));
+                .xpath("//label[@class='label-radio' and contains(., 'Selen')]//input"));
         if (!selenRadio.isSelected()) {
             selenRadio.click();
         }
 
         // 8. Select in dropdown
         WebElement dropdownYellow = driver.findElement(By
-                .xpath("//select[@class='uui-form-element' and contains(., 'Yellow')]"));
+                .xpath("//select[@class='uui-form-element' and contains(., 'Yellow')]//input"));
         dropdownYellow.click();
 
         // 9.1 Assert that for each checkbox there is an individual log row
         // and value is corresponded to the status of checkbox
-        assertFalse(waterCheckbox.isSelected());
+        assertTrue(waterCheckbox.isSelected());
         WebElement logWaterCheckbox = driver.findElement(By.xpath("//li[contains(., 'Water')]"));
         assertTrue(logWaterCheckbox.getText().contains("Water"));
 
-        assertFalse(windCheckbox.isSelected());
+        assertTrue(windCheckbox.isSelected());
         WebElement logWindCheckbox = driver.findElement(By.xpath("//li[contains(., 'Wind')]"));
         assertTrue(logWindCheckbox.getText().contains("Wind"));
 
         // 9.2 Assert that for radio button there is a log row and value is corresponded to the status of radio button
-        assertFalse(selenRadio.isSelected());
+        assertTrue(selenRadio.isSelected());
         WebElement logSelenRadio = driver.findElement(By.xpath("//li[contains(., 'Selen')]"));
         assertTrue(logSelenRadio.getText().contains("Selen"));
 
         // 9.3 Assert that for dropdown there is a log row and value is corresponded to the selected value
-        assertFalse(dropdownYellow.isSelected());
+        assertTrue(dropdownYellow.isSelected());
         WebElement logdropdownYellow = driver.findElement(By.xpath("//li[contains(., 'Yellow')]"));
         assertTrue(logdropdownYellow.getText().contains("Yellow"));
 
